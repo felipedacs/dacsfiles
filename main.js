a10be5024a9b8da5a86153f1be9592f05caf6027
Felipe Silva <felipe.dacs@gmail.com>
2018-10-20 13:39:14 -0300

    correção do botão de copy

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 var btnLubuntu = document.getElementById("btn-lubuntu");
 
 btnLubuntu.addEventListener("click", function(){
+    // estilização do btn
     btnLubuntu.classList.add('btn-active');
     btnLubuntu.value = "copied";
+
+    // pega texto e transfere para textarea
     let code = document.getElementById("code-lubuntu").innerHTML;
-    document.getElementById("tacopy").innerHTML = code;
     let textarea = document.getElementById("tacopy");
+    textarea.innerHTML = code;
+    //let textarea = document.getElementById("tacopy");
+
+    // envia objeto para ser copiado
     clipBoard(textarea)
+
+    // desfaz estilização
+    setTimeout(function(){ 
+        btnLubuntu.classList.remove('btn-active');
+        btnLubuntu.value = "again?";
+    }, 3000);
+
 });
 
 function clipBoard(textarea) {
+    console.log(textarea) ;
     textarea.select();
     document.execCommand("copy");
 }
